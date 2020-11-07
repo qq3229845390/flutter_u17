bf9ce1ac0c5b5aa053f029fc2e26a84aac9a4b2e
杨英俊 <>
2020-11-07 19:37:58 +0800

    完成我的界面

--- flutter_u17/lib/views/me/state.dart
+++ flutter_u17/lib/views/me/state.dart
@@ -2,12 +2,34 @@ import 'package:fish_redux/fish_redux.dart';
 
 class MeState implements Cloneable<MeState> {
 
+  List cellList;
+
   @override
   MeState clone() {
-    return MeState();
+    return MeState()
+      ..cellList = cellList
+    ;
   }
 }
 
 MeState initState(Map<String, dynamic> args) {
-  return MeState();
+  List cellList = [
+    {'icon': 'mine_vip', 'title': '我的VIP', 'padding': false},
+    {'icon': 'mine_coin', 'title': '充值妖气币', 'padding': true},
+    {'icon': 'mine_accout', 'title': '消费记录', 'padding': false},
+    {'icon': 'mine_subscript', 'title': '我的订阅', 'padding': false},
+    {'icon': 'mine_seal', 'title': '我的封印图', 'padding': true},
+    {'icon': 'mine_message', 'title': '我的消息/优惠券', 'padding': false},
+    {'icon': 'mine_cashew', 'title': '妖果商城', 'padding': false},
+    {'icon': 'mine_freed', 'title': '在线阅读免流量', 'padding': true},
+    {'icon': 'mine_feedBack', 'title': '帮助中心', 'padding': false},
+    {'icon': 'mine_mail', 'title': '我要反馈', 'padding': false},
+    {'icon': 'mine_judge', 'title': '给我们评分', 'padding': false},
+    {'icon': 'mine_author', 'title': '成为作者', 'padding': false},
+    {'icon': 'mine_setting', 'title': '设置', 'padding': true}
+  ];
+
+  return MeState()
+    ..cellList = cellList
+  ;
 }
--- flutter_u17/lib/views/me/view.dart
+++ flutter_u17/lib/views/me/view.dart
@@ -1,13 +1,104 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_u17/common/constants.dart';
 
 import 'action.dart';
 import 'state.dart';
 
 Widget buildView(MeState state, Dispatch dispatch, ViewService viewService) {
+
+  Widget _buildCell(item, index) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          height: 40,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('${Constants.AssetImage(item['icon'])}'),
+                width: 30, height: 30,
+              ),
+              Expanded(
+                child: Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(
+                        color: Color(0xfff4f4f4),
+                      width: item['padding']?0:1
+                    ))
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(item['title'], style: TextStyle(
+                        fontSize: 15
+                      ),),
+                      Icon(Icons.keyboard_arrow_right, color: Colors.grey,)
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Offstage(
+          offstage: !item['padding'],
+          child: Container(height: 10, color: Color(0xfff4f4f4),),
+        )
+      ],
+    );
+  }
+
   return Scaffold(
-    body: Center(
-      child: Text('我的'),
+    body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image(
+                  image: AssetImage('${Constants.AssetImage('mine_bg_for_boy')}'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ];
+        },
+        body: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: state.cellList.length,
+          itemBuilder: (context, index) {
+            return _buildCell(state.cellList[index], index);
+          },
+        )),
+  );
+
+  return Scaffold(
+    body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          primary: false,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image(
+              image: AssetImage('${Constants.AssetImage('mine_bg_for_boy')}'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return _buildCell(state.cellList[index], index);
+            },
+            childCount: state.cellList.length
+          ),
+        )
+      ],
     ),
   );
 }
